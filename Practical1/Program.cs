96dc0b4e248bbc634d2b1955df5dd32bd4848b5f
Evgen <dapodobral@gmail.com>
2023-05-30 12:25:37 +0300

    Add access modifiers

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -1,20 +1,30 @@
-﻿Console.WriteLine("String:");
-string line = Console.ReadLine();
-Int32 max = 0;
-Int32 count = 1;
-
-for (int i = 1; i < line.Length; i++)
+﻿public class Counter
 {
-    if (line[i] != line[i - 1])
+    public static void Main()
     {
-        count++;
-    }
-    else count = 1;
-    if (count > max)
-    {
-        max = count;
+        Console.WriteLine("String:");
+        string line = Console.ReadLine();
+        Int32 max = 0;
+        Int32 count = 1;
+
+        for (int i = 1; i < line.Length; i++)
+        {
+            if (line[i] != line[i - 1])
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+            }
+
+            if (count > max)
+            {
+                max = count;
+            }
+        }
+
+        Console.WriteLine($"length = " + line.Length);
+        Console.WriteLine($"There is {max} different symbols in a row");
     }
 }
-
-Console.WriteLine($"length = " + line.Length);
-Console.WriteLine($"There is {max} different symbols in a row");
